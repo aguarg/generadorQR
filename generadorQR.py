7094cd49d0977c83fc9005cdfc1b6e8a04bfb564
aguarg <93261481+aguarg@users.noreply.github.com>
2021-11-30 20:00:11 -0300

    Update generadorQR.py

--- generadorQR.py
+++ generadorQR.py
@@ -1,12 +1,22 @@
 import qrcode
 
-
-# Generando la imagen del código QR con la información que queramos:
-
+# Pregunta que texto queremos codificar:
 texto = input("Introduzca el mensaje a codificar: ")
 
-imagen = qrcode.make(texto)
 
+# Clase con valores para generar la imagen:
+qr = qrcode.QRCode(
+    version=None, #especifica el tamaño. Con None es flexible.
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=20,
+    border=4,
+)
+
+# Generamos la imagen con el texto indtroducido:
+qr.add_data(texto)
+qr.make(fit=True)
+
+imagen = qr.make_image(fill_color="black", back_color="white")
 
 
 # Guardamos la imagen generada, con el nombre de archivo que queramos:
